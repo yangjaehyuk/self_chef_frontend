f0985c99e25f7616cee989fea28dbb57e77230e7
yangjaehyuk <gndksakxk10@naver.com>
2022-11-18 13:37:38 +0900

    성별부분 체크박스로 바꿈, 식단페이지에서 개인정보 수정하는 페이지 추가

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -13,6 +13,9 @@ const output = {
     select: (req, res) => {
         res.render("home/select");
     },
+    setting: (req, res) => {
+        res.render("home/setting");
+    },
 };
 
 module.exports = {
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/select", ctrl.output.select);
+router.get("/setting", ctrl.output.setting);
 
 //외부로 내보내기
 module.exports = router;
