95a2360b1942ab871ea725f7497a98eb01b1fe84
yangjaehyuk <gndksakxk10@naver.com>
2022-12-03 17:11:19 +0900

    마이페이지 수정, 세팅부분 항목 추가

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -11,7 +11,7 @@ router.get("/select", ctrl.output.select);
 router.get("/setting", ctrl.output.setting);
 router.get("/community", ctrl.output.community);
 router.get("/mypage", ctrl.output.mypage);
-router.get("/fix_comment", ctrl.output.fix_comment);
 router.get("/write", ctrl.output.write);
+router.get("/mypage_setting", ctrl.output.mypage_setting);
 //외부로 내보내기
 module.exports = router;
